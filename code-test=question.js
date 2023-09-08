cd47481e7aea1a21c3acd714c2033e5e7357eb38
David05 <davidgreendevlops@gmail.com>
2023-09-08 03:38:41 +0100

    first code test question and answer

---
+++ code-test=question.js
@@ -0,0 +1,21 @@
+// Question 1 Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+// Example : For picture = ["abc", "ded"]the output should be
+// solution(picture) = ["*****", "*abc*", "*ded*", "*****"]
+function solution(picture) {
+    let asterisks = "*"
+    let arrayOfConcatenatedString = []
+    let arrayOfRepeatedAsterisks = []
+    let numbersOfCharactersToMakeARectangle
+    numbersOfCharactersToMakeARectangle = parseInt(picture.map(x => x.length + 2))
+    const repeatedAsterisks = asterisks.repeat(numbersOfCharactersToMakeARectangle)
+    arrayOfRepeatedAsterisks.push(repeatedAsterisks)
+    for (let kkk = 0; kkk < picture.length; kkk++) {
+        if (picture[kkk].length < numbersOfCharactersToMakeARectangle) {
+            let concatTheStrings = picture[kkk].concat(asterisks)
+            concatTheStrings = asterisks + concatTheStrings.charAt(0) + concatTheStrings.slice(1)
+            arrayOfConcatenatedString.push(concatTheStrings)
+        }
+    }
+
+    return [...arrayOfRepeatedAsterisks, ...arrayOfConcatenatedString, ...arrayOfRepeatedAsterisks]
+}
