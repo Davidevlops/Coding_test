143683ed3d300a9b726ee1913a29abaa18547f89
David05 <davidgreendevlops@gmail.com>
2023-12-25 16:48:52 +0100

    Changes

--- ppp.js
+++ ppp.js
@@ -1,40 +1,8 @@
-// The goal of this exercise is to convert a string to a new string where each
-// character in the new string is "(" if that character appears only once in the original string, or ")"
-// if that character appears more than once in the original string. Ignore capitalization when determining if a
-// character is a duplicate.
-function duplicateEncode(word) {
-// ...
-let wordToLowerCase = word.toLowerCase();
-let countKeeper = {};
-for (let i = 0; i <= wordToLowerCase.length - 1; i++) {
-if (isNaN(countKeeper[wordToLowerCase[i]])) {
-countKeeper[wordToLowerCase[i]] = 0;
-}
-countKeeper[wordToLowerCase[i]]++;
-}
-let finalAnswer = ``;
-for (let i = 0; i <= wordToLowerCase.length - 1; i++) {
-finalAnswer += countKeeper[wordToLowerCase[i]] > 1 ? ")" : "(";
-}
-return finalAnswer;
-}
-"console.log(duplicateEncode("edueu"));" 
-"console.log(duplicateEncode("edueu"));" 
-"console.log(duplicateEncode("edueu"));" 
-"console.log(duplicateEncode("edueu"));" 
-"console.log(duplicateEncode("edueu"));" 
-"console.log(duplicateEncode("edueu"));" 
-"console.log(duplicateEncode("edueu"));" 
-"console.log(duplicateEncode("edueu"));" 
-newString 
-newString 
-newString 
-newString 
-newString 
-newString 
-newString 
-newString 
-"// console.log(duplicateEncode(""edueu""));" 
-"// console.log(duplicateEncode(""edueu""));" 
-"// console.log(duplicateEncode(""edueu""));" 
-"// console.log(duplicateEncode(""edueu""));" 
+Editing file in loop iteration 1...
+// console.log(duplicateEncode("edueu")
+Editing file in loop iteration 2...
+// console.log(duplicateEncode("edueu")
+Editing file in loop iteration 3...
+// console.log(duplicateEncode("edueu")
+Editing file in loop iteration 4...
+// console.log(duplicateEncode("edueu")
